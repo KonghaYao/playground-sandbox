dc72a10edc309f47dd4a935ae53fe9a9ed209926
konghayao <3446798488@qq.com>
2022-06-29 14:54:09 +0800

    feat: 添加文件列表

--- src/sandbox.tsx
+++ src/sandbox.tsx
@@ -3,19 +3,50 @@ import {
     /* @ts-ignore  */
 } from "../rollup-web/dist/adapter/Fetcher/FSFetcher.js";
 
-import { Component } from "solid-js";
+import {
+    Component,
+    createEffect,
+    createMemo,
+    createResource,
+    createSignal,
+    For,
+} from "solid-js";
 import { IframeFactory } from "./LoadFile";
 
+/* 用于承载 Iframe */
 const Shower: Component<{
     port?: MessagePort;
     ready: (el: HTMLDivElement) => void;
 }> = (props) => {
     return <div ref={(el) => props.ready(el)}></div>;
 };
+export { FS };
+const FileSystem: Component<{
+    fs: FS;
+}> = (props) => {
+    const [path, setPath] = createSignal("/");
+    const [fileList, { refetch: reLoadFileList }] = createResource(() => {
+        return props.fs.promises.readdir(path()) as string[];
+    });
+    const enter = (item: string) => {
+        setPath(`${path()}${item}/`);
+        reLoadFileList();
+    };
+    return (
+        <div>
+            <div>{path}</div>
+            <ul>
+                <For each={fileList()}>
+                    {(item) => <li onclick={() => enter(item)}>{item}</li>}
+                </For>
+            </ul>
+        </div>
+    );
+};
 
 export const Sandbox: Component<{
     storeTag?: string;
-}> = (props) => {
+}> = (props = {}) => {
     const fs = new FS(props.storeTag || "_rollup_web_store_");
     let port: MessagePort | undefined = undefined;
     /* 加载文件的方式 */
@@ -25,9 +56,9 @@ export const Sandbox: Component<{
 
     /* 用于创建 Iframe 对象 */
     const createIframe = IframeFactory(loadFile);
-
     return (
         <div>
+            <FileSystem fs={fs}></FileSystem>
             <Shower port={port} ready={createIframe}></Shower>
         </div>
     );
--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,11 @@
-import { fs, Sandbox } from "../src/sandbox";
+import { FS, Sandbox } from "../src/sandbox";
 import { render } from "solid-js/web";
+const fs = new FS("_rollup_web_store_");
 const fileReflect = async (realPath: string, innerPath: string) => {
     const config = await fetch(realPath).then((res) => res.text());
     await fs.promises.writeFile(innerPath, config);
 };
 await fileReflect("./test/worker.js", "/rollup.config.web.js");
 await fileReflect("./test/index.html", "/index.html");
-
+console.log(fs);
 render(Sandbox as any, document.body);
