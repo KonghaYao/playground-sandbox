88313480c7b6b3006ea5270b896a4d80b455e459
konghayao <3446798488@qq.com>
2022-07-03 21:44:50 +0800

    feat: 添加项目 CDN

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,56 @@
 import alias from "@rollup/plugin-alias";
 import babel from "@rollup/plugin-babel";
 import nodeResolve from "@rollup/plugin-node-resolve";
-import fs from "fs";
+import fs, { writeFileSync } from "fs";
 import postcss from "rollup-plugin-postcss";
-
+import commonjs from "@rollup/plugin-commonjs";
+// import { terser } from "rollup-plugin-terser";
+import analyze from "rollup-plugin-analyzer";
 // rollup.config.js
 export default {
-    external: ["vscode-icons-js"],
+    external: [
+        "vscode-icons-js",
+        "solid-js",
+        "solid-js/web",
+        "@monaco-editor/loader",
+    ],
     input: "./test/index.ts",
     output: {
         file: "./dist/test.js",
         format: "es",
-        paths: {
-            "vscode-icons-js": "https://esm.run/vscode-icons-js",
+        path: {
+            // 这个库 40k
+            "vscode-icons-js": "https://cdn.skypack.dev/vscode-icons-js",
+            // 两个主要的库也可以使用 cdn
+            "solid-js": "https://cdn.skypack.dev/solid-js",
+            "solid-js/web": "https://cdn.skypack.dev/solid-js/web/dist/web.js",
+            "@monaco-editor/loader":
+                "https://cdn.skypack.dev/@monaco-editor/loader",
         },
     },
     plugins: [
+        {
+            resolveId(thisFile) {
+                if (thisFile.startsWith("https://")) {
+                    return false;
+                }
+            },
+        },
         alias({
             entries: [
-                // 这个库的导出出现了问题，我们将其导出改为 Index.ts
                 {
-                    find: "vscode-icons-js",
-                    replacement: "vscode-icons-js/src/Index",
+                    find: "rollup-web",
+                    replacement: true
+                        ? "https://cdn.jsdelivr.net/npm/rollup-web"
+                        : "./rollup-web",
                 },
             ],
         }),
         nodeResolve({
+            browser: true,
             extensions: [".ts", ".tsx", ".js"],
         }),
+        commonjs(),
         // 解决 svg 模块问题
         {
             name: "svg",
@@ -45,7 +68,12 @@ export default {
         postcss(),
         babel({
             babelHelpers: "bundled",
-            extensions: [".js", ".ts", ".tsx", ".svg"],
+            extensions: [".ts", ".tsx", ".js", ".svg"],
+        }),
+        // terser(),
+        analyze({
+            summaryOnly: true,
+            writeTo: (str) => writeFileSync("dist/index.analyze.txt", str),
         }),
     ],
 };
--- src/Helper.ts
+++ src/Helper.ts
@@ -1,8 +1,8 @@
 /* @ts-ignore  */
-import { FS as _FS } from "../rollup-web/dist/adapter/Fetcher/FSFetcher.js";
+import { FS as _FS } from "rollup-web/dist/adapter/Fetcher/FSFetcher.js";
 import A from "@isomorphic-git/lightning-fs";
 export const FS: typeof A = _FS;
 /* @ts-ignore */
-export { IframeEnv } from "../rollup-web/dist/Iframe.js";
+export { IframeEnv } from "rollup-web/dist/Iframe.js";
 /* @ts-ignore */
-export { Evaluator } from "../rollup-web/dist/index.js";
+export { Evaluator } from "rollup-web/dist/index.js";
