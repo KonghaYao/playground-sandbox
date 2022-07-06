df7e3312c2f685ab335caf1c26b6802d974f72ab
konghayao <3446798488@qq.com>
2022-07-06 11:03:15 +0800

    feat: 构建 expose api

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import { Sandbox } from "./sandbox";
 import { FS } from "./Helper";
 export { FS };
 import _FS from "@isomorphic-git/lightning-fs";
+
+type TreeObject = {
+    [ket: string]: TreeObject | string;
+};
+
+/* 不建议用户直接使用 FS，使用我们提供的 API 接口较好 */
 class FileAPI {
     constructor(public fs: _FS) {}
     /* 将 web 地址映射到 fs 中 */
@@ -16,15 +22,30 @@ class FileAPI {
             : fetch(webPath).then((res) => res.text()));
         await this.fs.promises.writeFile(fsPath, config);
     }
+    /* 将树状结构映射到 fs 中 */
+    async mergeTree(tree: TreeObject, root = "") {
+        const promises = Object.entries(tree).map(async ([key, value]) => {
+            if (typeof value === "string") {
+                return this.reflect(root + key, value);
+            } else {
+                return this.mergeTree(value, root + key + "/");
+            }
+        });
+        await Promise.all(promises);
+        return;
+    }
 }
 
-export const createSandbox = (
+export const createSandbox = async (
     element: HTMLElement,
     props: {
         storeTag?: string;
+        beforeMount?: (api: FileAPI) => void | Promise<void>;
     } = {}
 ) => {
     const fs = new FS(props.storeTag || "_rollup_web_store_");
+    const api = new FileAPI(fs);
+    props.beforeMount && (await props.beforeMount(api));
     render(() => Sandbox({ fs }), element);
-    return new FileAPI(fs);
+    return api;
 };
--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,17 @@
-import { FS, createSandbox } from "../src/index";
-const fs = new FS("_rollup_web_store_");
-const fileReflect = async (realPath: string, innerPath: string) => {
-    const config = await fetch(realPath).then((res) => res.text());
-    await fs.promises.writeFile(innerPath, config);
-};
-await fileReflect("./test/worker.js", "/rollup.config.web.js");
-await fileReflect("./test/index.html", "/index.html");
-console.log(fs);
-createSandbox(document.body);
+import { createSandbox } from "../src/index";
+
+createSandbox(document.body, {
+    async beforeMount(api) {
+        await api.reflect("./test/worker.js", "/rollup.config.web.js");
+        await api.reflect("./test/index.html", "/index.html");
+        // api.mergeTree(
+        //     {
+        //         test: {
+        //             "worker.js": "/rollup.config.web.js",
+        //             "index.html": "/index.html",
+        //         },
+        //     },
+        //     "./"
+        // );
+    },
+});
