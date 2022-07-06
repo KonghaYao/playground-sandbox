56dae6c39c1f99aa9ef2d16f7176487d561bb123
konghayao <3446798488@qq.com>
2022-07-06 15:35:06 +0800

    refractor: 封装文件编辑器逻辑

--- src/sandbox.tsx
+++ src/sandbox.tsx
@@ -11,13 +11,16 @@ export type SandboxInput = {
     fs: FS;
     files: string[][];
 };
-export const Sandbox: Component<SandboxInput> = (props) => {
+type Expose = {
+    watchingEditor: any;
+};
+const FileEditorList: Component<{
+    files: string[][];
+    fs: FS;
+    expose: (data: Expose) => void;
+}> = (props) => {
     const fs = props.fs;
     const [fileList, setFileList] = createSignal(props.files);
-    /* 加载文件的方式 */
-    const loadFile = async (url: string) => {
-        return fs.promises.readFile(url, "utf8") as Promise<string>;
-    };
     const save = (model: FileModel) => {
         fs.promises.writeFile(model.path, model.model.getValue());
         console.log("写入 ", model.path, "成功");
@@ -34,48 +37,66 @@ export const Sandbox: Component<SandboxInput> = (props) => {
             manager.hub.on("save", save);
         }
     );
-
+    props.expose({ watchingEditor });
     onCleanup(() => {
         ControllerList.forEach((manager) => manager.hub.off("save", save));
     });
+    return (
+        <div class={style.editor_list}>
+            <header class={style.editor_header}>
+                <div>File Editor</div>
+                <span></span>
+            </header>
+            {/* 使用循环遍历构建多个 Editor */}
+            <For each={fileList()}>
+                {(el) => {
+                    return (
+                        <FileEditor
+                            fileList={el}
+                            getFile={getFile}
+                            closeSelf={() => {
+                                const newList = fileList().filter(
+                                    (i) => !Object.is(i, el)
+                                );
+                                setFileList(newList);
+                            }}
+                        ></FileEditor>
+                    );
+                }}
+            </For>
+        </div>
+    );
+};
 
+export const Sandbox: Component<SandboxInput> = (props) => {
+    /* 加载文件的方式 */
+    const loadFile = async (url: string) => {
+        return props.fs.promises.readFile(url, "utf8") as Promise<string>;
+    };
+    let watchingEditor: any;
     return (
         <>
             <sl-split-panel class={style.sandbox}>
                 <div class={style.file_box} slot="start">
                     <FileExplorer
-                        fs={fs}
+                        fs={props.fs}
                         openFile={(path) => {
-                            fs.promises.readFile(path, "utf8").then((res) => {
-                                watchingEditor
-                                    .getWatching()
-                                    .openFile(path, res as any as string);
-                            });
+                            props.fs.promises
+                                .readFile(path, "utf8")
+                                .then((res) => {
+                                    watchingEditor
+                                        .getWatching()
+                                        .openFile(path, res as any as string);
+                                });
                         }}
                     ></FileExplorer>
-                    <div class={style.editor_list}>
-                        <header>
-                            <div>File Editor</div>
-                            <span></span>
-                        </header>
-                        {/* 使用循环遍历构建多个 Editor */}
-                        <For each={fileList()}>
-                            {(el) => {
-                                return (
-                                    <FileEditor
-                                        fileList={el}
-                                        getFile={getFile}
-                                        closeSelf={() => {
-                                            const newList = fileList().filter(
-                                                (i) => !Object.is(i, el)
-                                            );
-                                            setFileList(newList);
-                                        }}
-                                    ></FileEditor>
-                                );
-                            }}
-                        </For>
-                    </div>
+                    <FileEditorList
+                        fs={props.fs}
+                        files={props.files}
+                        expose={(data) => {
+                            watchingEditor = data.watchingEditor;
+                        }}
+                    ></FileEditorList>
                 </div>
                 <div class={previewStyle.previewer} slot="end">
                     <Previewer loadFile={loadFile}></Previewer>
