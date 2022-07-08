9474152d055391603428f2939dd26a8f31b129fb
konghayao <3446798488@qq.com>
2022-07-08 15:25:22 +0800

    feat: 优化导入方式

--- src/FileEditor/getMonaco.tsx
+++ src/FileEditor/getMonaco.tsx
@@ -23,23 +23,28 @@ let loading: Promise<typeof wrapper["monaco"]>;
 export const getMonaco = async () => {
     if (loading) return loading;
     loading = (async () => {
-        await loadMonaco();
-        await loadScript(
-            "https://cdn.jsdelivr.net/npm/vscode-oniguruma/release/main.js"
-        );
+        await Promise.all([
+            loadMonaco(),
+            loadScript(
+                "https://cdn.jsdelivr.net/npm/vscode-oniguruma/release/main.js"
+            ),
+        ]);
         wrapper = await import("@codingame/monaco-editor-wrapper");
-        await import(
-            "@codingame/monaco-editor-wrapper/dist/features/htmlContribution"
-        );
-        await import(
-            "@codingame/monaco-editor-wrapper/dist/features/typescriptContribution"
-        );
-        await import(
-            "@codingame/monaco-editor-wrapper/dist/features/cssContribution"
-        );
-        await import(
-            "@codingame/monaco-editor-wrapper/dist/features/extensionConfigurations"
-        ); /* @ts-ignore*/
+        await Promise.all([
+            import(
+                "@codingame/monaco-editor-wrapper/dist/features/htmlContribution"
+            ),
+            import(
+                "@codingame/monaco-editor-wrapper/dist/features/typescriptContribution"
+            ),
+            import(
+                "@codingame/monaco-editor-wrapper/dist/features/cssContribution"
+            ),
+            import(
+                "@codingame/monaco-editor-wrapper/dist/features/extensionConfigurations"
+            ),
+        ]);
+        /* @ts-ignore*/
         globalThis.monaco = wrapper.monaco;
         return wrapper.monaco;
     })();
